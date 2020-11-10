1977715b25b80d887729c699360015259b633262
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-09 16:56:55 -0800

    Fixed Route error, added static, displayed quantity and total attribute in template, and added secure route on back end to manage the price of items by id number

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -8,9 +8,23 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
+    if 'orders' not in request.session:
+        request.session['orders'] = int(request.POST["quantity"])
+    if 'total' not in request.session:
+        request.session['total'] = Product.objects.get(id=int(request.POST["price"])).price
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
+    price_from_form = float(Product.objects.get(id=int(request.POST["price"])).price)
     total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
+    print("Charging credit card...", total_charge)
+    request.session['orders'] += quantity_from_form
+    request.session['total'] += total_charge
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return redirect('checkout/processed')
+
+def process(request):
+    context = {
+        'item': Order.objects.last(),
+        'orders': request.session['orders'],
+        'total': request.session['total']
+    }
+    return render(request, "store/checkout.html", context)
